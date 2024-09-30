31497474080bf5c494f007d411c7471a2a118180
Your Name <vasavilakka398@gmail.com>
2024-09-30 21:12:36 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from app.models import*
 # Create your views here.
+ 
+def insert_Dept(request):
+    dno=int(input('enter dept no: '))
+    dn=input('enter dept name: ')
+    dl=input('enter dept loc: ')
+    DO=Dept.objects.get_or_create(deptno=dno, dname=dn, dloc=dl)
+    if DO[1]:
+        return HttpResponse('New object is created')
+    else:
+        return HttpResponse("Already Exits")
+def insert_Emp(request):
+    dno=int(input('enter dept no: '))
+    dn=input('enter dept name: ')
+    dl=input('enter dept loc: ')
+    eno=int(input('enter emp no: '))
+    en=input('enter emp name: ')
+    ej=input('enter emp designation: ')
+    es=input('enter emp sal: ')
+    hd=input('enter hire date: ')
+    c=int(input('enter comm: '))
+    m=input('enter manager: ')
+
+    DO=Dept.objects.get_or_create(deptno=dno, dname=dn, dloc=dl)[0]
+    EO=Emp.objects.get_or_create(deptno=DO, empno=eno, ename=en, job=ej, sal=es, hiredate=hd,comm=c, mgr=m)
+    if EO[1]:
+        return HttpResponse('New object is created')
+    else:
+        return HttpResponse('Already exits')
+
+
+
--- project17/urls.py
+++ project17/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('insert_Dept/', insert_Dept, name='insert_Dept'),
+    path('insert_Emp/', insert_Emp, name='insert_Emp'),
 ]
